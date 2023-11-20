c026e03adfb4b6be0bf9a8dc39746d25949446b6
PayalAhirwar <payalahirwar03@gmail.com>
2023-11-20 10:07:06 +0530

    added question model file

--- backend/models/question.js
+++ backend/models/question.js
@@ -5,10 +5,6 @@ const questionSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  // options: {
-  //   type: [String],
-  //   required: true,
-  // },
   options: [
     {
       text: String,
@@ -25,11 +21,6 @@ const questionSchema = new mongoose.Schema({
     type: Number,
     required: true,
    }
-  // quiz: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: 'newQuiz', // Reference to the newQuiz model
-  // },
-  // // Add any other fields you need for your questions
 });
 
 const Question = mongoose.model('Question', questionSchema);
